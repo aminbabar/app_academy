9279784b98ec7aed1b83e8c6ac5767b725a932c5
Amin <amin.babar1@gmail.com>
2021-06-21 14:17:56 -0400

    completed monkey patching project

--- W2D1/monkey_patching_project/lib/array.rb
+++ W2D1/monkey_patching_project/lib/array.rb
@@ -6,10 +6,12 @@ class Array
     self.max - self.min
   end
 
+  
   def average
     return nil if self.empty?
     self.sum / (self.length / 1.0)
   end
+
 
   def median
     return nil if self.empty?
@@ -20,6 +22,7 @@ class Array
     end
   end
 
+
   def counts
     count = Hash.new(0)
     self.each {|ele| count[ele] += 1}
@@ -27,5 +30,35 @@ class Array
   end
 
 
-  
+  def my_count(val)
+    count = 0
+    self.each {|ele| count += 1 if ele == val}
+    count
+  end
+
+
+  def my_index(val)
+    self.each_with_index {|ele, i| return i if val == ele }
+    nil
+  end
+
+
+  def my_uniq   #!!!!!!
+    hash = Hash.new(0)
+    self.each {|ele| hash[ele] += 1}
+    hash.keys
+  end
+
+
+  def my_transpose
+    new_arr = Array.new (self[0].length) {[]}
+    (0...self.length).each do |i|
+        (0...self.length).each do |j|
+            new_arr[j] << self[i][j]
+        end
+    end
+    new_arr
+  end
+
+
 end
