e070b53b1e45ec32365ce9e61a8fa8147b24d770
Amin <amin.babar1@gmail.com>
2021-06-21 11:42:30 -0400

    done with boot camp project

--- W2D1/bootcamp_project/lib/bootcamp.rb
+++ W2D1/bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,70 @@
+require "byebug"
+
 class Bootcamp
-  
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers, @students = [], []
+    @grades = Hash.new {|hash, key| hash[key] = []}
+  end
+
+  def name
+    @name
+  end
+
+  def slogan
+    @slogan
+  end
+
+  def teachers
+    @teachers
+  end
+
+  def students
+    @students
+  end
+
+  def hire(teacher)
+    @teachers << teacher
+  end
+
+
+  def enroll(student)
+    if @student_capacity > @students.length
+        @students << student
+        return true
+    else
+        return false
+    end
+  end
+
+
+  def enrolled?(student)
+    @students.include?(student)
+  end
+
+
+  def student_to_teacher_ratio
+    @students.length / @teachers.length
+  end
+
+  def add_grade(student, grade)
+    if @students.include?(student)
+        @grades[student] << grade
+        return true
+    else
+        return false
+    end
+  end
+
+  def num_grades(student)
+    @grades[student].length
+  end
+
+  def average_grade(student)
+    return nil if !self.enrolled?(student) || self.num_grades(student) == 0
+    @grades[student].sum / num_grades(student)
+  end
+
 end
