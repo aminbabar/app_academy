dc666c7ed4d5aeeb53a438cb35ae70f210cd4280
Amin <amin.babar1@gmail.com>
2021-06-23 19:19:25 -0400

    mastermind complete

--- W2D3/mastermind_project/lib/code.rb
+++ W2D3/mastermind_project/lib/code.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Code
   attr_reader(:pegs)
 
@@ -45,5 +46,29 @@ class Code
     @pegs.length
   end
 
+  def num_exact_matches(code_instance)
+    num = 0
+    (0...code_instance.length).each {|i| num += 1 if code_instance[i] == @pegs[i] }
+    num
+  end
+
+  def num_near_matches(code_instance)
+    near_matches = 0
+    count = Hash.new(0)
+    code_instance.pegs.each {|peg| count[peg] += 1}
+    @pegs.each_with_index do |peg, i|
+      if count[peg] >= 1 && peg != code_instance.pegs[i]
+        count[peg] -= 1
+        near_matches += 1
+      elsif @pegs[i] == peg
+        count[peg] -= 1
+      end
+    end
+    near_matches
+  end
+
+  def ==(code_instance)
+    @pegs == code_instance.pegs
+  end
 
 end
--- W2D3/mastermind_project/lib/mastermind.rb
+++ W2D3/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,20 @@
 require_relative "code"
 
 class Mastermind
+    def initialize(length)
+        @secret_code = Code.random(length)
+    end
+
+    def print_matches(code_instance)
+        print "exact matches: #{code_instance.num_exact_matches(@secret_code)}"
+        print "near matches: #{code_instance.num_near_matches(@secret_code)}"
+    end
+
+    def ask_user_for_guess
+        puts "Enter a code"
+        user_input_code = Code.from_string(gets.chomp)
+        print_matches(user_input_code)
+        user_input_code.num_exact_matches(@secret_code) == user_input_code.length
+    end
 
 end
