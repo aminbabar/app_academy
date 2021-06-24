a0157f14fe464aa366590c73e0f91bbeea8c6133
Amin <amin.babar1@gmail.com>
2021-06-23 20:44:46 -0400

    completed part 1 of battleship

--- W2D3/battleship_project/lib/board.rb
+++ W2D3/battleship_project/lib/board.rb
@@ -1,3 +1,27 @@
 class Board
-  
+    attr_reader(:size)
+
+    def initialize(n)
+        @grid = Array.new(n) {Array.new(n, :N)}
+        @size = n * n
+    end
+
+    def [](arr)
+        @grid[arr[0]][arr[1]]
+    end
+
+    def []=(pos, value)
+        x, y = pos
+        @grid[x][y] = value
+    end
+
+    def num_ships
+        count = 0
+        @grid.each do |arr|
+            arr.each {|ele| count += 1 if ele == :S}
+        end
+        count
+    end
+
+    
 end
